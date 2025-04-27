811df30114771cc4dced6d4f26c2fde070de8f3d
Soney <sonephyo7777777@gmail.com>
2025-04-26 20:54:11 -0400

    update: configure the backend to handle server connection

--- backend/main.py
+++ backend/main.py
@@ -5,47 +5,58 @@ from flask_cors import CORS
 from flask_socketio import join_room, leave_room
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}) 
+CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+
 
 @app.route("/")
 def hello():
     return "Hello, this is the Flask-SocketIO server."
 
-# @socketio.on("connect")
-# def on_connect():
-#     print("Client connected:", request.sid)
-    
-# @socketio.on("disconnect")
-# def handle_disconnect():
-#     print(f"User {request.sid} disconnected.")
 
-# @socketio.on("message")
-# def handle_message(data):
-#     print(f"Received message: {data}")
-#     emit("response", f"Server received: {data}", broadcast=True)
+@socketio.on("connect")
+def on_connect():
+    print("Client connected:", request.sid)
 
 
-#  The following codes are for room related socket handlers
-@socketio.on('join')
+@socketio.on("disconnect")
+def handle_disconnect():
+    print(f"User {request.sid} disconnected.")
+
+
+chat_history = {}
+
+
+@socketio.on("join")
 def on_join(data):
-    username = data['username']
-    room = data['room']
+    username = data["username"]
+    room = data["room"]
     join_room(room)
-    send(username + ' has entered the room.', to=room)
+    send(username + " has entered the room.", to=room)
 
-@socketio.on('leave')
+    if room in chat_history:
+        for message in chat_history[room]:
+            emit("receive_message", {"message": message}, to=request.sid)
+
+
+@socketio.on("leave")
 def on_leave(data):
-    username = data['username']
-    room = data['room']
+    username = data["username"]
+    room = data["room"]
     leave_room(room)
-    send(username + ' has left the room.', to=room)
-    
-@socketio.on('send_message')
+    send(username + " has left the room.", to=room)
+
+
+@socketio.on("send_message")
 def handle_send_message(data):
-    message = data['message']
-    room = data['room']
-    emit('receive_message', {'message': message}, to=room)
+    message = data["message"]
+    room = data["room"]
+    if room not in chat_history:
+        chat_history[room] = []
+    chat_history[room].append(message)
+
+    emit("receive_message", {"message": message}, to=room, broadcast=True)
+
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5001, debug=True)
